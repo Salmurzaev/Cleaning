cc515975dab6868670358950766dfad77a8b28f1
Dastan <salmurzaev1st@gmail.com>
2023-06-04 00:53:04 +0600

    last2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,13 @@
 import '../styles/globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Уборка и клининг в Бишкеке по цене от 39 сом за м2',
   description:
-    'Клининговая компания «Clean House» осуществляет услуги по уборке квартир, офисов, коттеджей, загородных домов, и помещений различного назначения в Бишкеке. Вы можете сделать заказ на профессиональный клининг у нас на сайте или позвонить по телефону.',
+    'Клининговая компания «Clean Time осуществляет услуги по уборке квартир, офисов, коттеджей, загородных домов, и помещений различного назначения в Бишкеке. Вы можете сделать заказ на профессиональный клининг у нас на сайте или позвонить по телефону.',
   keywords: 'уборка квартир, клининг бишкек, заказать клининг, доступная цена'
 }
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
     <html lang='ru' className='smooth-scroll'>
       <body className={inter.className} suppressHydrationWarning={true}>
         {children}
+        <Analytics />
       </body>
     </html>
   )
